763e5516ccdc8687f3f9a166bc82ce4645ca0f2d
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-09-08 12:45:45 +0100

    added test for multiply

--- test/test.js
+++ test/test.js
@@ -1,19 +1,24 @@
 let sum = require("../lib/sum");
 let sub = require("../lib/sub");
+let multiply = require("../lib/multiply");
 let { expect } = require("chai");
 
 describe("sum", function () {
-  // test a functionality
   it("should add numbers", function () {
-    // add an assertion
     expect(sum(1, 20)).to.equal(21);
   });
 });
 
 describe("sub", function () {
-  // test a functionality
   it("should subtract numbers", function () {
-    // add an assertion
     expect(sub(20, 1)).to.equal(19);
+  });
+});
+
+describe("multiply", function () {
+  it("should multiply numbers", function () {
+    expect(multiply(20, 1)).to.equal(20);
+    expect(multiply(2, 2)).to.equal(4);
+    expect(multiply(15, 2)).to.equal(30);
   });
 });
